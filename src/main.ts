8cb12bbc60e654878b5ba672c2549899171f3618
cuiyiming1998 <1269567664@qq.com>
2023-01-31 15:42:34 +0800

    :recycle: refactor(main): 删除isReady

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 import '@arco-design/web-vue/dist/arco.css'
 import App from './App.vue'
 import { setupStore } from './store'
-import router, { setupRouter } from './router/index'
+import { setupRouter } from './router/index'
 import 'animate.css'
 import i18n from './locale'
 import 'uno.css'
@@ -13,7 +13,5 @@ app.use(ArcoVueIcon)
 app.use(i18n)
 await setupStore(app)
 await setupRouter(app)
-
-await router.isReady()
 
 app.mount('#app')
